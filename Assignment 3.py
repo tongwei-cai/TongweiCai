d8491dac04a8ca52809336d3cf84b9cf147a125d
tongwei-cai <65097306+tongwei-cai@users.noreply.github.com>
2020-05-09 20:41:07 -0400

    Create Assignment 3.py

---
+++ Assignment 3.py
@@ -0,0 +1,13 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Family = ['Cai', 'Kevin', 'Keisuke', 'Louis', 'JS']
+>>> Hero = ['A', 'B', 'C', 'D']
+>>> import random
+>>> def hello():
+	print(random.choice(Family + Hero))
+
+	
+>>> hello()
+Kevin
+>>> 
